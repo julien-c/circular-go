1ef97466533b67b87a9e9279c4795c8a305d6a55
Julien Chaumond <chaumond@gmail.com>
2013-12-09 17:45:44 +0100

    [API] api/counter

---
+++ counter.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"labix.org/v2/mgo"
+)
+
+
+type Counter struct {
+	Count int `json:"count"`
+}
+
+
+var session, _ = mgo.Dial("localhost")
+var c = session.DB("circular").C("posts")
+
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	count, _ := c.Count()
+	m := Counter{count}
+	b, _ := json.Marshal(m)
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+}
+
+func main() {
+	http.HandleFunc("/api/counter", serve)
+	http.ListenAndServe(":8080", nil)
+}
+
