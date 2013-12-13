64e0b2f816ee7ef58e05f8160f07f763be489668
Julien Chaumond <chaumond@gmail.com>
2013-12-13 16:52:22 +0100

    go fmt

--- counter.go
+++ counter.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"net/http"
 	"encoding/json"
 	"labix.org/v2/mgo"
+	"net/http"
 )
-
 
 type Counter struct {
 	Count int `json:"count"`
 }
 
-
 var session, _ = mgo.Dial("localhost")
 var c = session.DB("circular").C("posts")
-
 
 func serve(w http.ResponseWriter, r *http.Request) {
 	count, _ := c.Count()
 	m := Counter{count}
 	b, _ := json.Marshal(m)
-	
+
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(b)
 }
@@ -29,4 +26,3 @@ func main() {
 	http.HandleFunc("/api/counter", serve)
 	http.ListenAndServe(":8080", nil)
 }
-
